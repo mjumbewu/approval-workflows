8e6ba6fed6a3dcd803cd045d268f4f79fa9bb0ce
Mjumbe Wawatu Ukweli <mjumbewu@gmail.com>
2016-03-09 11:28:07 -0500

    Move the package initialization from tasks to models

--- src/kibali/models.py
+++ src/kibali/models.py
@@ -1,6 +1,16 @@
+import re
+from os.path import join as pathjoin
+from uuid import uuid4
+from django.conf import settings
 from django.db import models
 from django.contrib.postgres.fields import ArrayField
 from django.utils.timezone import now
+from django.utils.translation import ugettext as _
+
+from logging import getLogger
+logger = getLogger(__name__)
+
+
 
 
 #
@@ -28,6 +38,35 @@ class Package (models.Model):
     # current_state = models.ForeignKey('State')
     # content = models.BinaryField()  # The tarred git repository
     repo = models.URLField(null=True, blank=True)
+
+    def _make_repo_url(self):
+        return pathjoin(settings.KIBALI_REPO_BASE, str(self.pk) + '.git')
+
+    def init(self, data='{}', commit=True):
+        logger.debug(('Initializing new repository for package with '
+                      'ID {}').format(package_pk))
+
+        with GitRepo.init() as repo:
+
+            # Create, add, and commit the data file to the repo
+            logger.debug('Adding and committing data to the repo at {}'
+                         .format(repo.path))
+            datafilename = pathjoin(repo.path, 'data.json')
+            with open(datafilename, 'w') as datafile:
+                datafile.write(data)
+            repo.add(datafilename)
+            repo.commit(message='Initial Commit')
+
+            # Store the repo
+            repo_url = self._make_repo_url()
+            logger.debug('Copying the bare repo at {}/.git to {}'
+                         .format(repo.path, repo_url))
+            copytree(pathjoin(repo.path, '.git'), repo_url)
+
+            # Save the repo location
+            self.repo = repo_url
+            if commit:
+                self.save()
 
     def sign(self, signing_actor, signing_datetime=None, ):
         if signing_datetime is None:
--- src/kibali/tasks.py
+++ src/kibali/tasks.py
@@ -1,5 +1,6 @@
 from celery import shared_task
 from gitutils import GitRepo
+from shutil import copytree
 from kibali.models import Package
 
 from logging import getLogger
@@ -8,20 +9,5 @@ logger = getLogger(__name__)
 
 @shared_task
 def create_package(package_pk, data):
-    logger.debug('Initializing new repository for package with ID {}'.format(
-        package_pk))
-
-    pacakge = Package.objects.get(pk=package_pk)
-
-    with GitRepo.init() as repo:
-
-        # Create, add, and commit the data file to the repo
-        datafilename = pathjoin(repo, 'data.json')
-        with open(datafilename, 'w') as datafile:
-            json.dump(data, datafile, sort_keys=True, indent=1)
-        repo.add(datafilename)
-        repo.commit(message='Initial Commit')
-
-        # Store the repo
-        repo.add_remote('origin', '')
-        repo.push()
+    package = Package.objects.get(pk=package_pk)
+    package.init(data=data)
