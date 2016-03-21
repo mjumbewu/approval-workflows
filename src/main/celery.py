f0bc04dbd1a2b4766038eb3a6fa5b6c2891ddb72
Mjumbe Wawatu Ukweli <mjumbewu@gmail.com>
2016-03-21 13:13:01 -0400

    Rename 'approval' app to 'main'

--- src/main/celery.py
+++ src/main/celery.py
@@ -10,11 +10,11 @@ import os
 from celery import Celery
 
 # set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'approval.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings')
 
 from django.conf import settings  # noqa
 
-app = Celery('approval')
+app = Celery('main')
 
 # Using a string here means the worker will not have to
 # pickle the object when using Windows.
